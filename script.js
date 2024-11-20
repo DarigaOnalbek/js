46ad6c00a171c54ec613ea763ce630c1b3aa5f9b
DarigaOnalbek <yourmyhope4@gmail.com>
2024-11-20 13:28:50 +0500

    js tasks

---
+++ script.js
@@ -0,0 +1,24 @@
+//medium tasks
+//1
+var celsia = 24
+
+var farengeit = (celsia * 9/5) + 32
+console.log("Температура в градусах Фаренгейта:" + farengeit)
+
+var celvin = celsia + 273.15
+console.log("Температура в градусах Кельвина:" + celvin)
+
+//2
+
+var deposit = 1000
+var rate = 0.05
+
+var summa1 = (deposit * rate) + deposit
+console.log("Сумма вклада через год:" + summa1)
+
+var summa2 = (summa1 * rate) + summa1
+console.log("Сумма вклада через два год:" + summa2)
+
+var summa3 = (summa2 * rate) + summa2
+console.log("Сумма вклада через три год:" + summa3)
+
